c15b8ae038edbd655d833bafa5b6d2b22f950ea1
Kellie-Ann Smith Scott <smithks2@yahoo.com>
2013-12-07 11:30:57 -0500

    Added permissions for user to see pantry inventory

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -10,14 +10,23 @@ class BootStrap {
 
         if(!User.count()) {
             //TODO: add salt
-            User smithScott  = new User(username: 'smithscott', passwordHash: new Sha256Hash('smithscott').toHex(), roles: [head]).save(failOnError: true)
+            User smithScott  = new User(username: 'smithscott', passwordHash: new Sha256Hash('smithscott').toHex(), roles: [head]).save()
 
-            User robinson = new User(username: 'robinson', passwordHash: new Sha256Hash('robinson').toHex(), roles: [head]).save(failOnError: true)
-            User geli = new User(username: 'geli', passwordHash: new Sha256Hash('geli').toHex(), roles: [member]).save(failOnError: true)
+            User robinson = new User(username: 'robinson', passwordHash: new Sha256Hash('robinson').toHex(), roles: [head]).save()
+            User geli = new User(username: 'geli', passwordHash: new Sha256Hash('geli').toHex(), roles: [member]).save()
 
-            User simo = new User(username: 'simo', passwordHash: new Sha256Hash('simo').toHex(), roles: [head]).save(failOnError: true)
-            User tar = new User(username: 'tar', passwordHash: new Sha256Hash('tar').toHex(), roles: [member]).save(failOnError: true)
-            User ang = new User(username: 'ang', passwordHash: new Sha256Hash('ang').toHex(), roles: [member]).save(failOnError: true)
+            User simo = new User(username: 'simo', passwordHash: new Sha256Hash('simo').toHex(), roles: [head]).save()
+            User tar = new User(username: 'tar', passwordHash: new Sha256Hash('tar').toHex(), roles: [member]).save()
+            User ang = new User(username: 'ang', passwordHash: new Sha256Hash('ang').toHex(), roles: [member]).save()
+
+            //permissions all users have:
+            User.list().each {User u->
+                //can access their own pantry
+                u.addToPermissions('pantryInventory:index')
+                u.addToPermissions('pantryInventory:list')
+                u.save()
+            }
+
         }
 
     }
