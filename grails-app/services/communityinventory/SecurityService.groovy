e5fc850316d22bef76f968343479c6889e3dd8ac
Kellie-Ann Smith Scott <smithks2@yahoo.com>
2013-12-09 17:23:07 -0500

    Attempt at authenticating, when api call.

--- grails-app/services/communityinventory/SecurityService.groovy
+++ grails-app/services/communityinventory/SecurityService.groovy
@@ -1,6 +1,7 @@
 package communityinventory
 
 import org.apache.shiro.SecurityUtils
+import org.apache.shiro.authc.UsernamePasswordToken
 import scott.kellie.User
 
 class SecurityService {
@@ -13,8 +14,18 @@ class SecurityService {
     User getCurrentUser() {
         User currentUser = null
         if (SecurityUtils.subject.isAuthenticated()) {
-            currentUser = User.findByUsername(SecurityUtils.subject.principal)
+            currentUser = User.findByUsername(SecurityUtils.subject.principal as String)
         }
         return currentUser
+    }
+
+    boolean apiLogin(String username, String password) {
+        UsernamePasswordToken token = new UsernamePasswordToken(username: username, password: password)
+        try {
+            SecurityUtils.subject.login(token)
+            return true
+        } catch (AuthenticationException){
+            return false
+        }
     }
 }
